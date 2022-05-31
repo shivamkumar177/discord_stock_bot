f5c1e783303ed0052072c87a05cdd1cb03fbaa9f
shivam <shivamk1717@gmail.com>
2022-05-31 23:41:14 +0530

    Added inital build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 import pandas as pd
-from yahoofinancials import yahoofinancials
+# from yahoofinancials import yahoofinancials
 
 tsla_stock = yf.download('TSLA',start='2022-01-01',end='2022-03-31')
 print(tsla_stock)
