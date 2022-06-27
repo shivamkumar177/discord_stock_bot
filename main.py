d50d84c3a00a38d8360c79c5decb112a6e395e9f
shivam <shivamk1717@gmail.com>
2022-06-28 01:02:56 +0530

    csv download from yahoo_finance

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import yfinance as yf
 import pandas as pd
+from pathlib import Path
 # from yahoofinancials import yahoofinancials
 
-tsla_stock = yf.download('TSLA',start='2022-01-01',end='2022-03-31')
-print(tsla_stock)
+stock = 'TSLA'
+output_file = '{}.csv'.format(stock)
+output_dir = Path('stock_csv')
+output_dir.mkdir(parents=True, exist_ok=True)
+tsla_stock = yf.download(stock,start='2022-01-01',end='2022-03-31')
+tsla_stock.to_csv(output_dir/output_file, encoding='utf-8')
