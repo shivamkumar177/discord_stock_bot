99526e20be3caa2d6877516a8ff7c8f8521648b7
shivam <shivamk1717@gmail.com>
2022-06-30 23:19:17 +0530

    added graph for dataframe

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+from tracemalloc import start
 import yfinance as yf
 import pandas as pd
 from pathlib import Path
+pd.options.plotting.backend = "plotly"
 # from yahoofinancials import yahoofinancials
+
+def download_file(stock, start_date, end_date):
+    return yf.download(stock,start=start_date,end=end_date)
 
 stock = 'TSLA'
 output_file = '{}.csv'.format(stock)
-output_dir = Path('stock_csv')
-output_dir.mkdir(parents=True, exist_ok=True)
-tsla_stock = yf.download(stock,start='2022-01-01',end='2022-03-31')
-tsla_stock.to_csv(output_dir/output_file, encoding='utf-8')
+csv_output_dir = Path('stock_csv')
+csv_output_dir.mkdir(parents=True, exist_ok=True)
+tsla_stock = download_file(stock, start_date='2022-06-18', end_date='2022-06-28')
+# plot graph
+fig = tsla_stock.plot()
+fig.show()
+png_output_dir = Path("stock_png")
+png_output_dir.mkdir(parents=True, exist_ok=True)
+png_output_file = "{}.png".format(stock)
+fig.write_image(png_output_dir/png_output_file)
+tsla_stock.to_csv(csv_output_dir/output_file, encoding='utf-8')
